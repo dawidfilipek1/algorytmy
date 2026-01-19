5671a4cb8d1aa6c47603d527f090b76c02a2b102
xdawix902 <dawid.filipek90@wp.pl>
2026-01-19 16:33:33 +0100

    e

---
+++ .history/naive_20260119163315.py
@@ -0,0 +1,9 @@
+def naive(lista):
+    counter = 0;
+
+    for i in range(0, len(lista)):
+        for j in range(i+1, len(lista)):
+            if(lista[i] > lista[j]):
+                counter+=1
+
+    return counter
